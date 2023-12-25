f0fc54b936171a796e01958a13490e87bd46134f
lalalafalah <muhfalahmubaraq@gmail.com>
2023-12-25 14:42:33 +0700

    edit dikit

--- header.php
+++ header.php
@@ -32,7 +32,7 @@ if (isset($_SESSION['kd_cs'])) {
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="#" style="color: #4942E4"><b>Toko Buku</b></a>
+				<a class="navbar-brand" href="#" style="color: #E33924"><b>IT BOOK STORE</b></a>
 			</div>
 
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
